01dff6a234e9e6d8109fba3871f5cac498f97f6d
Baha134 <soskabach@gmail.com>
2026-06-07 17:08:50 +0500

    fix10

---
+++ fix-modal.js
@@ -0,0 +1,6 @@
+﻿const fs = require('fs');
+let f = fs.readFileSync('client/src/pages/LeadsPage.jsx', 'utf8');
+f = f.replace("alignItems: 'center', justifyContent: 'center', padding: '20px',", "alignItems: 'flex-start', justifyContent: 'center', padding: '40px 20px', overflowY: 'auto',");
+f = f.replace("maxWidth: '600px', maxHeight: '90vh', overflowY: 'auto',", "maxWidth: '600px', marginBottom: '40px',");
+fs.writeFileSync('client/src/pages/LeadsPage.jsx', f);
+console.log('Done');
