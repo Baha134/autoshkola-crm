87d87594b4ef9c21bbc38ff3369903be8061cd97
Baha134 <soskabach@gmail.com>
2026-06-07 17:11:22 +0500

    fix11

---
+++ fix-modal2.js
@@ -0,0 +1,5 @@
+﻿const fs = require('fs');
+let f = fs.readFileSync('client/src/pages/LeadsPage.jsx', 'utf8');
+f = f.replace("padding: '40px 20px', overflowY: 'auto',", "padding: '40px 20px 40px', overflowY: 'scroll',");
+fs.writeFileSync('client/src/pages/LeadsPage.jsx', f);
+console.log('Done');
